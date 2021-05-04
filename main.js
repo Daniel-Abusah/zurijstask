d54fe45c2e0c482ccc8c71c475f6c74495f47404
Daniel_Kormi <72416953+Daniel-Abusah@users.noreply.github.com>
2021-05-04 08:23:22 +0000

    Add files via upload
    making the navbar responsive using js

---
+++ main.js
@@ -0,0 +1,17 @@
+const navbar = document.querySelector(".div navbar);
+        const navbarMenu = document.querySelector(".div navbar");
+        const navbarLinks = document.querySelectorAll(".div navbar a");
+
+        navbar.addEventListener("click, .div navbar click");
+
+        function.div navbar click() {
+            navbarLinks.classList.navbarLinks("open-navbarLinks")
+            navbarMenu.classList.navbarLinks("open")
+            navbarLinks.forEach(element)
+
+            function navbarLinksclick() {
+                if (navbarMenu.classList.contains("open")) {
+                    .div navbar.click();
+                }
+            }
+        }
